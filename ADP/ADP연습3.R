841c4424c21826768a339a3ec784bdd8ed162fa8
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-07-10 19:50:22 +0900

    Update ADP연습3.R

--- ADP/ADP연습3.R
+++ ADP/ADP연습3.R
@@ -10,5 +10,5 @@ sink()
 list.files() # 파일 목록 출력
 list.files(recursive = T, all.files = T) # 모든 파일 리스트
 
-source("C:/자료실/서다현/R 스크립트/10.R")
+source("C:/10.R") # 디렉토리
 write.csv(transaction, "test.csv", row.names = F) # csv 파일 생성
