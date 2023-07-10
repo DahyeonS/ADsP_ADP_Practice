3245a1643f2478da0e537a29edca3c9f703dcd84
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2023-07-10 19:49:53 +0900

    Update ADP연습1.R

--- ADP/ADP연습1.R
+++ ADP/ADP연습1.R
@@ -2,7 +2,7 @@ install.packages("fBasics")
 library(help = fBasics)
 help(package = fBasics)
 
-source("C:/자료실/서다현/R 스크립트/5.R") # 스크립트 파일 실행
+source("C:/5.R") # 스크립트 파일 실행 # 디렉토리
 
 sink("result.txt", append = FALSE, split = TRUE) # 파일을 생성해서 결과값을 보냄
 a = 1
